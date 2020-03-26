afe25cbd430dcfb8189219222fcd0073e4a11bf4
Svante Krusberg <svante.krusberg@elev.ga.ntig.se>
2020-03-26 14:52:05 +0100

    weeee

---
+++ Prr02-Slutprojekt-FiskeSpel/Prr02-Slutprojekt-FiskeSpel/Fisk.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prr02_Slutprojekt_FiskeSpel
+{
+    class Fisk
+    {
+        int size;
+        int value;
+        int rarity;
+        string[] rarityLabel = { "common", "uncommon", "rare", "epic", "legendary" };
+
+        public Fisk()
+        {
+            DeclareRarity();
+        }
+
+        public void DeclareRarity()
+        {
+            int r = Utils.rng.Next(100);
+            if(r > 60)
+            {
+                rarity = 0;
+                Console.WriteLine(r);
+            }
+            else if(r > 30 && r < 60)
+            {
+                rarity = 1;
+                Console.WriteLine(r);
+            }
+            else if(r > 10 && r < 30)
+            {
+                rarity = 2;
+                Console.WriteLine(r);
+            }
+            else if(r < 10)
+            {
+                rarity = 3;
+                Console.WriteLine(r);
+            }
+            else
+            {
+                rarity = 4;
+                Console.WriteLine(r);
+            }
+
+        }
+
+        public void PrintStats()
+        {
+            Console.WriteLine(rarityLabel[rarity]);
+            Console.WriteLine(rarity);
+
+        }
+
+    }
+}
---
+++ Prr02-Slutprojekt-FiskeSpel/Prr02-Slutprojekt-FiskeSpel/Player.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prr02_Slutprojekt_FiskeSpel
+{
+    class Player
+    {
+        public string Input()
+        {
+            string userInput;
+            bool correctInput = false;
+
+            while(correctInput == false)
+            {
+                
+                userInput = Console.ReadLine();
+                userInput.ToLower().Trim();
+                if(userInput == "a"|| userInput == "b")
+                {
+                    return userInput;
+                    
+
+                }
+                else
+                {
+                    
+                }
+
+                return userInput;
+            }
+
+        }
+
+    }
+}
